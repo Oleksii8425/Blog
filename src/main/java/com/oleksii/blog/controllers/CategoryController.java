4318ce9f49c06e8fe93208f1b8c2af1201da84e7
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-06-02 12:26:45 +0100

    add CategoryController

---
+++ src/main/java/com/oleksii/blog/controllers/CategoryController.java
@@ -0,0 +1,32 @@
+package com.oleksii.blog.controllers;
+
+import com.oleksii.blog.domain.dtos.CategoryDto;
+import com.oleksii.blog.domain.entities.Category;
+import com.oleksii.blog.mappers.CategoryMapper;
+import com.oleksii.blog.services.impl.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    private final CategoryMapper categoryMapper;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDto>> listCategories() {
+        List<CategoryDto> categories = categoryService.listCategories()
+                .stream().map(categoryMapper::toDto)
+                .toList();
+        return ResponseEntity.ok(categories);
+    }
+
+}
