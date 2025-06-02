b3a6e03510fcbfd3bfee25490fd8f6c539338428
Oleksii Pohorilyi <oleksiy678@gmail.com>
2025-06-02 12:49:38 +0100

    add crateCategory() method inside CategoryService to create a new Category

--- src/main/java/com/oleksii/blog/services/impl/CategoryService.java
+++ src/main/java/com/oleksii/blog/services/impl/CategoryService.java
@@ -3,6 +3,7 @@ package com.oleksii.blog.services.impl;
 import com.oleksii.blog.domain.entities.Category;
 import com.oleksii.blog.repositories.CategoryRepository;
 import com.oleksii.blog.services.ICategoryService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +18,16 @@ public class CategoryService implements ICategoryService {
     @Override
     public List<Category> listCategories() {
         return categoryRepository.findAllWithPostCount();
+    }
+
+    @Override
+    @Transactional// makes multiple calls to a database (lines 27 and 31) happen in a single transaction
+    public Category createCategory(Category category) {
+        String categoryName = category.getName();
+        if (categoryRepository.existsByNameIgnoreCase(categoryName)) {
+            throw new IllegalArgumentException("Category already exists with name: " + categoryName);
+        }
+
+        return categoryRepository.save(category);
     }
 }
